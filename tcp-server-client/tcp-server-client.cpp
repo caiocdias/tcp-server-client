292b7fc2e2eefa381c748a9fab1ec2f4cf75357f
Caio Cezar Dias <caiocd007@gmail.com>
2024-10-27 20:40:20 -0300

    Adicionar arquivos de projeto.

---
+++ tcp-server-client/tcp-server-client.cpp
@@ -0,0 +1,12 @@
+﻿// tcp-server-client.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "tcp-server-client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
